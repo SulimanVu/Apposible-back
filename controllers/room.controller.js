b537bd32278647c36ab764b16b551fa28a8b56d0
Suliman <sssuliman@bk.ru>
2022-11-10 14:27:06 +0300

    Подправил controller

--- controllers/room.controller.js
+++ controllers/room.controller.js
@@ -3,39 +3,59 @@ const User = require("../models/User.model");
 
 module.exports.roomController = {
   createRoom: async (req, res) => {
-    const data = await Room.create({
-      name: req.body.name,
-      users: req.params.id,
-    });
+    try {
+      const data = await Room.create({
+        name: req.body.name,
+        users: req.params.id,
+      });
 
-    res.json(data);
+      res.json(data);
+    } catch (error) {
+      res.json(error);
+    }
   },
   getRoom: async (req, res) => {
-    const data = await Room.find();
-    res.json(data);
+    try {
+      const data = await Room.find();
+      res.json(data);
+    } catch (error) {
+      res.json(error);
+    }
   },
   deleteRoom: async (req, res) => {
-    const data = await Room.findByIdAndDelete(req.params.id);
-    res.json(data);
+    try {
+      const data = await Room.findByIdAndDelete(req.params.id);
+      res.json(data);
+    } catch (error) {
+      res.json(error);
+    }
   },
   addUserRoom: async (req, res) => {
-    const data = await Room.findByIdAndUpdate(req.params.id, {
-      $addToSet: { users: req.body.users },
-    });
-    const result = await data.populate("users");
-    res.json(result);
+    try {
+      const data = await Room.findByIdAndUpdate(req.params.id, {
+        $addToSet: { users: req.body.users },
+      });
+      const result = await data.populate("users");
+      res.json(result);
+    } catch (error) {
+      res.json(error);
+    }
   },
   deleteUserRoom: async (req, res) => {
-    const room = await Room.findById(req.params.id);
+    try {
+      const room = await Room.findById(req.params.id);
 
-    const user = await room.users.find((item) => {
-      return item.toString() === req.body.users;
-    });
+      const user = await room.users.find((item) => {
+        return item.toString() === req.body.users;
+      });
 
-    const data = await Room.updateOne(room, {
-      $pull: { users: user },
-    });
+      const data = await Room.updateOne(room, {
+        $pull: { users: user },
+      });
 
-    res.json(data);
+      res.json(data);
+    } catch (error) {
+      res.json(error);
+    }
   },
 };
